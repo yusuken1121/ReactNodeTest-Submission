883ff73fcfc926ca0f40f0037767ce7d9bfc843b
yusuken1121 <yusuken1121@gmail.com>
2025-07-29 19:24:35 +0900

    Define meeting routes

--- Server/controllers/meeting/_routes.js
+++ Server/controllers/meeting/_routes.js
@@ -1,5 +1,13 @@
 const express = require('express');
+const meeting = require('./meeting');
 
 const router = express.Router();
+
+router.post('/add', meeting.add);
+router.get('/', meeting.index);
+router.get('/:id', meeting.view);
+router.put('/edit/:id', meeting.edit);
+router.delete('/delete/:id', meeting.deleteData);
+router.post('/deleteMany', meeting.deleteMany);
 
 module.exports = router;
