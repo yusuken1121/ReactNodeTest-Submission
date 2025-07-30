ff8ffd6a0b8e0a9e6fc9fb97f39379ad7bc4d9ad
yusuken1121 <yusuken1121@gmail.com>
2025-07-30 21:25:01 +0900

    Fix Meeting API endpoints and controller

--- Server/controllers/meeting/_routes.js
+++ Server/controllers/meeting/_routes.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const meeting = require('./meeting');
+const auth = require('../../middelwares/auth');
 
 const router = express.Router();
 
-router.post('/add', meeting.add);
-router.get('/', meeting.index);
-router.get('/:id', meeting.view);
-router.put('/edit/:id', meeting.edit);
-router.delete('/delete/:id', meeting.deleteData);
-router.post('/deleteMany', meeting.deleteMany);
+router.post('/add', auth, meeting.add);
+router.get('/', auth, meeting.index);
+router.get('/view/:id', auth, meeting.view);
+router.put('/edit/:id', auth, meeting.edit);
+router.delete('/delete/:id', auth, meeting.deleteData);
+router.post('/deleteMany', auth, meeting.deleteMany);
 
 module.exports = router;
